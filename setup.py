3719754105e283844b8286df7a578b0a762011bd
sabarinathan <94899972+sarag5@users.noreply.github.com>
2024-09-23 13:09:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mlcookiecutter",
+    version="0.1.0",
+    author="sarag5",
+    author_email="sabarinathan_96@proton.me",
+    description="A tool to create project structures for ml/data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sarag5/MLCookiecutter",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "click",
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "project_creator=project_creator.main:create_project",
+        ],
+    },
+)
